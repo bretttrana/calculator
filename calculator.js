2a6daca7c03345c8056a5af72bde56103b681ae3
bretttrana <brett.trana@gmail.com>
2016-10-17 22:45:40 +0000

    Working Calculator

--- calculator.js
+++ calculator.js
@@ -1,7 +1,20 @@
-
 var keys = document.querySelector('#keys');
 
 var calcLine = '';
+var first;
+var oper;
+var second;
+
+var screen = document.createElement('ul');
+var screenLi = document.createElement('li');
+screenLi.setAttribute("id", "textScreen");
+screen.appendChild(screenLi);
+var clearLi = document.createElement('input');
+clearLi.setAttribute("type", "button");
+clearLi.setAttribute("id", "clear");
+clearLi.setAttribute("value", "C")
+clearLi.setAttribute("onclick", "clearCalcLine()");
+screen.appendChild(clearLi);
 
 var buttons = document.createElement('ul');
 
@@ -12,15 +25,55 @@ for (var i=0; i<buttonsSymbols.length; i++) {
     var button = document.createElement('input');
     button.setAttribute("type", "button");
     button.setAttribute("value", buttonsSymbols[i]);
-    button.setAttribute("onclick", "appendVal()");
+    button.setAttribute("onclick", "appendVal(this.id)");
+    button.setAttribute('id', i);
     li.appendChild(button);
     buttons.appendChild(li);
 }
 
-function appendVal() {
-    calcLine += button.value;
+function appendVal(clicked_id) {
+    if (buttonsSymbols[clicked_id] == '+' || buttonsSymbols[clicked_id] == '-' || 
+        buttonsSymbols[clicked_id] == 'x' || buttonsSymbols[clicked_id] == '/') {
+            first = calcLine;
+            oper = buttonsSymbols[clicked_id];
+            calcLine = "";
+    }
+    else {
+        calcLine += buttonsSymbols[clicked_id];
+        document.getElementById('textScreen').textContent = calcLine;
+    }
+    if (buttonsSymbols[clicked_id] == '=') {
+        second = calcLine;
+        if (oper == '+') {
+            calcLine = parseFloat(first) + parseFloat(second);
+        }
+        if (oper == '-') {
+            calcLine = parseFloat(first) - parseFloat(second);
+        }
+        if (oper == 'x') {
+            calcLine = parseFloat(first) * parseFloat(second);
+        }
+        if (oper == '/') {
+            calcLine = parseFloat(first) / parseFloat(second);
+        }
+        calcLine = +calcLine.toFixed(6);
+        first = calcLine;
+        oper = "";
+        document.getElementById('textScreen').textContent = calcLine;
+    }
+}
+function clearCalcLine() {
+    calcLine = "";
+    first = 0;
+    oper = "";
+    second = 0;
+    document.getElementById('textScreen').textContent = calcLine;
 }
 
+function newEntry() {
+    calcLine = "";
+    document.getElementById('textScreen').textContent = calcLine;
+}
 
+keys.appendChild(screen);
 keys.appendChild(buttons);
-
